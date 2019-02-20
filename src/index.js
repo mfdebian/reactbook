6b83dc7e17dd1bc187aa999734194faaa33dc020
Alfredo González <alfredo.arosteguy@gmail.com>
2019-02-20 13:46:25 -0300

    First commit - React Hello World :)

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+// App is the App component from App.js
+// root is the div from index.html with id="root"
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
